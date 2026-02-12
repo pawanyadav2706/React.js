c6ef1add34d29d9123de475b8a18d23a1144d9b8
pawanyadav2706 <pawanyadav12582@gmail.com>
2026-02-12 19:43:41 +0530

    Add files via upload
    learning react. and element create in the react so code upload the file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+// const element = <h1 className='color'>Welcome to my React app and my first React Project</h1>;
+// ReactDOM.createRoot(document.getElementById('root')).render(element);
+// const Newelement = <h1 className='color'>understanding in element to crate the react app...</h1>;
+// ReactDOM.createRoot(document.getElementById('name')).render(Newelement);
+
+// const element =(
+//   <div className='color'>
+//     <h1>welcome to my first react app..</h1>
+//     <h2>Learning react app..</h2>
+//   </div>
+// )
+// ReactDOM.createRoot(document.getElementById('root')).render(element);
+
+
+// create element using react without using jsx
+// const element = React.createElement('h1', {className: 'color'}, 'Welcome to my React app and my first React Project');
+// ReactDOM.createRoot(document.getElementById('root')).render(element);
+
+
+// create element using jsx without using react
+
+const element = React.createElement('div',{className: 'color'},
+  React.createElement('h1', null, 'Welcome to my React app and my first React Project'),
+  React.createElement('h2', null, 'Learning react app..')
 );
+ReactDOM.createRoot(document.getElementById('root')).render(element);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
